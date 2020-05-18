a951d09650825f68c8450a0022f2130258898c3e
Aureate Team <hello@aureatelabs.com>
2020-05-18 14:44:07 +0530

    Add a default store ad testimonial

--- packages/shopify/theme/plugins/shopify.js
+++ packages/shopify/theme/plugins/shopify.js
@@ -1,9 +1,7 @@
 import { setup } from '@vue-storefront/shopify-api';
 
 const config = {
-  // domain: 'testimonial-aula.myshopify.com',
-  // storefrontAccessToken: '29d77b8cb02a1b019fb50e57c7249936'
-  domain: 'lauridukaan.myshopify.com',
-  storefrontAccessToken: '1daf49e906c0433bae0335731776facf'
+  domain: 'testimonial-aula.myshopify.com',
+  storefrontAccessToken: '29d77b8cb02a1b019fb50e57c7249936'
 };
 setup(config);
