40502ed12c40f6ff92d219f8709747365f8cb827
Aureate Team <hello@aureatelabs.com>
2020-06-11 12:29:33 +0530

    Completed pages and article module

---
+++ packages/shopify/theme/pages/blog/_slug.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="single-article">
+    <div class="image">
+      <img :src="blog.articleByHandle.image.originalSrc" />
+    </div>
+    <div class="header">
+      <h4>{{ blog.articleByHandle.title }}</h4>
+      <small>{{ blog.articleByHandle.publishedAt }} Posted by {{ blog.articleByHandle.authorV2.name }}</small>
+    </div>
+    <div class="description" v-html="blog.articleByHandle.contentHtml" />
+  </div>
+</template>
+<script>
+
+import { useArticle } from '@vue-storefront/shopify';
+
+export default {
+  async asyncData ({ params }) {
+    const { blogs } = await useArticle({ slug: params.slug });
+    return { blog: blogs[0] }
+  }
+};
+
+</script>
+
+<style lang="scss">
+.single-article {
+  margin-bottom: 25px;
+  .header {
+    padding: 25px 0 0 0;
+    margin-bottom: 0;
+    h4 {
+      font-size: 28px;
+      font-weight: normal;
+    }
+  }
+  .description {
+    padding: 25px 0 0 0;
+    line-height: 26px;
+    color: #595c65;
+  }
+  .image {
+    max-height: 500px;
+    overflow: hidden;
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
