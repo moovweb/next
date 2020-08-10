b568e8a34504df44aaa3ff89562bfe1c99602f57
Mark Brocato <mark.brocato@moovweb.com>
2020-08-10 13:51:56 +0300

    working on xdn

--- packages/shopify/theme/lang/config.js
+++ packages/shopify/theme/lang/config.js
@@ -1,31 +1,31 @@
 const locales = [
   {
-    code: 'en',
-    file: 'en.js',
-    name: 'en',
-    label: 'English',
+    code: "en",
+    file: "en.js",
+    name: "en",
+    label: "English",
     shopId: 121,
     country: {
-      name: 'US',
-      label: 'United States'
+      name: "US",
+      label: "United States"
     },
     currency: {
-      name: 'USD',
-      label: 'Dollar'
+      name: "USD",
+      label: "Dollar"
     }
   }
 ];
 
-export default {
+module.exports = {
   locales,
   defaultLocale: locales[0].code,
   lazy: true,
-  langDir: 'lang/',
+  langDir: "lang/",
   vueI18n: {
     fallbackLocale: locales[0].code
   },
   detectBrowserLanguage: {
-    cookieKey: 'vsf-locale',
+    cookieKey: "vsf-locale",
     alwaysRedirect: true
   }
 };
---
+++ packages/shopify/theme/routes.js
@@ -0,0 +1,10 @@
+// This file was automatically added by xdn deploy.
+// You should commit this file to source control.
+
+const { Router } = require("@xdn/core/router");
+const { createNuxtPlugin } = require("@xdn/nuxt");
+const { nuxtMiddleware } = createNuxtPlugin();
+
+module.exports = new Router().use(nuxtMiddleware).fallback(({ proxy }) => {
+  proxy("__js__");
+});
